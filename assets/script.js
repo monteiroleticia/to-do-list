ca6127f7f60e71440139474ce984d9890ce6f5b7
monteiroleticia <mntrltc@gmail.com>
2019-04-30 18:54:30 -0300

    atualiza comentários

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,22 @@
 
+// links com o html 
 const submitForm = document.getElementById('form');
 const inputTask = document.getElementById('input');
 const listContainer = document.getElementById('listContainer');
 
 submitForm.addEventListener('submit', function(e){
-    e.preventDefault();
+    e.preventDefault(); // previne que o submit apague o input
     
     const newTask = inputTask.value;
+    
+    // verifica que o input é válido
     if(newTask != ""){
 
+    // cria a lista com as tarefas
     const itemTask = document.createElement('div');
     itemTask.innerHTML = `<p>${newTask}</p><button>x</button>`;
     listContainer.appendChild(itemTask);
-    
+
     }
 
 });
